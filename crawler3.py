2163a52363a2b29f3f262bf1d64c84e2e2686b85
ericgr3gory <eric@e-gregory.com>
2024-01-23 20:18:33 +0000

    changes

--- crawler3.py
+++ crawler3.py
@@ -13,6 +13,7 @@ def find_links(parsed_html_data):
         links.append(a.get("href"))
     return links
 
+
 def parse_links(list_links):
     links_cleaned = []
     list_links = set(list_links)
@@ -28,35 +29,28 @@ def parse_page(page_data):
     parsed_url_data = BeautifulSoup(page_data, 'html.parser')
     return parsed_url_data
 
-def flatten_list(all_the_links):
-    flattened = []
-    
-    for l in all_the_links:
-        flattened.extend(l)
-    
-    return list(set(flattened))
-
 
 def main():
-    root_url = 'https://docs.chia.net'
-    links = "/"
+    url = "https://docs.chia.net/introduction/"
+    page = request_page(url)
+    page = parse_page(page)
+    links = find_links(page)
+    links = parse_links(links)
     all_the_links = [links]
-    visited_links = []
-    while set(links) != set(visited_links):
+    visited_links = [url]
+    for _ in range(6):
         for link in links:
             if link not in visited_links:
                 visited_links.append(link)
-                page = request_page(f'{root_url}{link}')
+                page = request_page(f'https://docs.chia.net{link}')
                 page = parse_page(page)
                 new_links = find_links(page)
-                new_links = parse_links(new_links)               
+                new_links = parse_links(links)               
                 all_the_links.append(new_links)
-    
-        print(links)
+
+        flattened_list = [x for sub_links in all_the_links for x in sub_links]
+        links = list(set(flattened_list))
         print(len(links))
-        
-        links = flatten_list(all_the_links)
-    
 
 
 if __name__ == "__main__":
