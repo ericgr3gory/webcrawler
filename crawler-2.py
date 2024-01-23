178a5abbbda45d3480f3ca8258211a6407efffc9
ericgr3gory <eric@e-gregory.com>
2024-01-23 21:24:28 +0000

    beeter stuff

--- crawler-2.py
+++ crawler-2.py
@@ -2,18 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def request_page(url):
+def request_page(url: str) -> str:
     r = requests.get(url)
     return r.text
 
 
-def find_links(parsed_html_data):
+def find_links(parsed_html_data: object) -> list:
     links = []
     for a in parsed_html_data.find_all('a'):
         links.append(a.get("href"))
     return links
 
-def parse_links(list_links):
+def parse_links(list_links: list) -> list:
     links_cleaned = []
     list_links = set(list_links)
     for link in list_links:
@@ -24,11 +24,11 @@ def parse_links(list_links):
     return links_cleaned
 
 
-def parse_page(page_data):
+def parse_page(page_data: str) -> object:
     parsed_url_data = BeautifulSoup(page_data, 'html.parser')
     return parsed_url_data
 
-def flatten_list(all_the_links):
+def flatten_list(all_the_links: list) -> list:
     flattened = []
     
     for l in all_the_links:
@@ -37,27 +37,31 @@ def flatten_list(all_the_links):
     return list(set(flattened))
 
 
-def main():
-    root_url = 'https://docs.chia.net'
-    links = "/"
+def crawl(root_url: str, seed_link: str)->list:    
+    links = seed_link
     all_the_links = [links]
     visited_links = []
+    
     while set(links) != set(visited_links):
+        
         for link in links:
+            
             if link not in visited_links:
                 visited_links.append(link)
-                page = request_page(f'{root_url}{link}')
-                page = parse_page(page)
-                new_links = find_links(page)
-                new_links = parse_links(new_links)               
+                page = parse_page(page_data=request_page(url=f'{root_url}{link}'))
+                new_links = parse_links(list_links=find_links(parsed_html_data=page))               
                 all_the_links.append(new_links)
     
-        print(links)
-        print(len(links))
-        
-        links = flatten_list(all_the_links)
+        links = flatten_list(all_the_links=all_the_links)
     
+    return links
 
+def main():
+    root_url = 'https://docs.chia.net'
+    seed_link = "/"
+    c = crawl(root_url=root_url, seed_link=seed_link)
+    print(c)
+    
 
 if __name__ == "__main__":
     main()
